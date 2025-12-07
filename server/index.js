5b0bc7a4b2ae152dd6b779fda6f6fca3080bd387
Liem <liemphan802@gmail.com>
2025-12-07 17:00:58 +1100

    chore: added routes folder.

--- server/index.js
+++ server/index.js
@@ -1,7 +1,9 @@
 import express from 'express';
+import auth from './routes/auth.js';
 
 const app = express();
 app.use(express.json());
+app.use('/api/auth', auth);
 
 app.listen(4000, () => console.log("Server started on port 4000"))
 export default app;
